825a1e8366d0002c4e48e19db8c3c71b406ac1df
eren.kaya <kayaom17@itu.edu.tr>
2023-08-07 01:07:21 +0300

    login sign-up

---
+++ src/pages/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SideMenu = () => {
+  return (
+    <ul>
+        <li>Settings</li>
+        <li>Profile</li>
+    </ul>
+  )
+}
+
+export default SideMenu;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,20 @@
 import { AppConfig } from "@/utils/AppConfig"
 import { Button, Grid, Stack } from "@mui/material"
 import Head from "next/head"
+import Header from "./components/Header/Header"
+import Dashboard from "./dashboard/Dashboard"
+import SideMenu from "./components/SideMenu/SideMenu"
 export default function Home() {
   return (
     <>
-      <Grid container height='100vh' alignItems='center' justifyContent='center' direction='column'>
-        <h1 className='text-white'>Next.js 13 with Tailwind and MUI Boilerplate</h1>
-        <Stack direction='row' columnGap={1}>
-          <Button variant='text' className='text-red-500'>
-            Text
-          </Button>
-          <Button variant='contained'>Contained</Button>
-          <Button variant='outlined'>Outlined</Button>
-        </Stack>
-      </Grid>
+      <Head>
+        <title>A/B Testing as SaaS</title>
+      </Head>
+      <main>
+        <Header/>
+        <Dashboard/>
+        <SideMenu/>
+      </main>
     </>
   )
 }
